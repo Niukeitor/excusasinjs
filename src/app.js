c47a91fc3dd5884a5f1e0d8925e079e2106b8081
Maikol Moreira <Niukeitor@hotmail.com>
2021-06-17 18:31:32 +0000

    subo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,46 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = genearExcusa();
+  });
+  console.log("Hello Rigor from  the console! ");
+};
+let genearExcusa = () => {
+  let pronoun = ["Un/a", "La/El"];
+  let subject = [
+    "mosca 🦟",
+    "gato 🐱",
+    "perro 🐶",
+    "payaso 🤡",
+    "abuelita 🤶",
+    "Mickey 🐭"
+  ];
+  let action = ["tomo mi", "tiró mi", "le grito a mi", "robo mi", "mordio mi"];
+  let possetion = [
+    "tarea 📖",
+    "auto 🚗",
+    "bicicleta 🚲",
+    "zapato 👞",
+    "dedo del pie 🦶"
+  ];
+  let where = ["en la calle.", "en mi casa. ", "en la entrada del colegio."];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
